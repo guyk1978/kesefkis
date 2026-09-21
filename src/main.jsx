6b0c3d9ae369c677739844453206d1dd71b41e9d
Guy Koren <dgartists@gmail.com>
2026-09-21 15:53:07 +0300

    Fix main.jsx import to load App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>💰 כסף כיס</h1>
-      <p>ברוכים הבאים ללוח העבודות והשירותים המקומי!</p>
-    </div>
-  )
-}
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
